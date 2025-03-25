994ef2bb53809353f22627174c2ddb8ca80be8f3
SOGLIUZZO Loris <sogliuzzo.loris@gmail.com>
2025-03-25 17:54:48 +0100

    TD6

--- TD6/exo1/muddy-city.c
+++ TD6/exo1/muddy-city.c
@@ -6,34 +6,102 @@
 #include "graphalg.h"
 #include "list.h"
 
-void print_graph (integer_graph_t* graph) {
-  generic_list_elmt_t* elem1 =
-    generic_list_head(&(graph->adjlists));
+void print_graph(integer_graph_t *graph)
+{
+  generic_list_elmt_t *elem1 =
+      generic_list_head(&(graph->adjlists));
   /* Boucle sur les elements de la liste */
-  for (; elem1 != NULL; elem1 = generic_list_next(elem1)) {
-    integer_adjlist_t* tempV1 =
-      (integer_adjlist_t*)generic_list_data(elem1);
-    printf ("Vertex %d: ", tempV1->vertex);
+  for (; elem1 != NULL; elem1 = generic_list_next(elem1))
+  {
+    integer_adjlist_t *tempV1 =
+        (integer_adjlist_t *)generic_list_data(elem1);
+    printf("Vertex %d: ", tempV1->vertex);
 
-    generic_list_elmt_t* elem2 =
-      generic_list_head(&(tempV1->adjacent));
+    generic_list_elmt_t *elem2 =
+        generic_list_head(&(tempV1->adjacent));
     /* Boucle sur les elements de l'ensemble (set) */
-    for (; elem2 != NULL; elem2 = generic_list_next(elem2)) {
+    for (; elem2 != NULL; elem2 = generic_list_next(elem2))
+    {
       integer_adjlist_elmt_t *tempV2 =
-	(integer_adjlist_elmt_t*)generic_list_data(elem2);
-      printf ("%d->", tempV2->vertex);
+          (integer_adjlist_elmt_t *)generic_list_data(elem2);
+      printf("%d->", tempV2->vertex);
     }
-    printf ("\n");
+    printf("\n");
   }
 }
 
-
-int main (int argc, char** argv) {
+int main(int argc, char **argv)
+{
   integer_graph_t graph;
-  integer_graph_init (&graph);
+  integer_graph_init(&graph);
 
-  /* TODO */
+  for (int i = 0; i < 10; i++)
+  {
+    integer_graph_ins_vertex(&graph, i);
+  }
 
-  integer_graph_destroy (&graph);
+  integer_graph_ins_edge(&graph, 0, 1, 5);
+  integer_graph_ins_edge(&graph, 0, 2, 3);
+  integer_graph_ins_edge(&graph, 0, 3, 4);
+
+  integer_graph_ins_edge(&graph, 1, 0, 5);
+  integer_graph_ins_edge(&graph, 1, 2, 3);
+  integer_graph_ins_edge(&graph, 1, 4, 2);
+  integer_graph_ins_edge(&graph, 1, 9, 4);
+  integer_graph_ins_edge(&graph, 1, 5, 3);
+
+  integer_graph_ins_edge(&graph, 2, 0, 3);
+  integer_graph_ins_edge(&graph, 2, 3, 5);
+  integer_graph_ins_edge(&graph, 2, 6, 4);
+  integer_graph_ins_edge(&graph, 2, 4, 3);
+  integer_graph_ins_edge(&graph, 2, 1, 3);
+
+  integer_graph_ins_edge(&graph, 3, 0, 4);
+  integer_graph_ins_edge(&graph, 3, 2, 5);
+  integer_graph_ins_edge(&graph, 3, 6, 4);
+  integer_graph_ins_edge(&graph, 3, 7, 2);
+
+  integer_graph_ins_edge(&graph, 4, 1, 2);
+  integer_graph_ins_edge(&graph, 4, 2, 3);
+  integer_graph_ins_edge(&graph, 4, 6, 4);
+  integer_graph_ins_edge(&graph, 4, 8, 3);
+  integer_graph_ins_edge(&graph, 4, 9, 3);
+
+  integer_graph_ins_edge(&graph, 5, 1, 3);
+  integer_graph_ins_edge(&graph, 5, 9, 2);
+
+  integer_graph_ins_edge(&graph, 6, 2, 4);
+  integer_graph_ins_edge(&graph, 6, 3, 4);
+  integer_graph_ins_edge(&graph, 6, 7, 3);
+  integer_graph_ins_edge(&graph, 6, 8, 2);
+  integer_graph_ins_edge(&graph, 6, 4, 4);
+
+  integer_graph_ins_edge(&graph, 7, 3, 2);
+  integer_graph_ins_edge(&graph, 7, 6, 3);
+  integer_graph_ins_edge(&graph, 7, 8, 4);
+
+  integer_graph_ins_edge(&graph, 8, 4, 3);
+  integer_graph_ins_edge(&graph, 8, 6, 2);
+  integer_graph_ins_edge(&graph, 8, 7, 4);
+  integer_graph_ins_edge(&graph, 8, 9, 4);
+
+  integer_graph_ins_edge(&graph, 9, 5, 2);
+  integer_graph_ins_edge(&graph, 9, 1, 4);
+  integer_graph_ins_edge(&graph, 9, 4, 3);
+  integer_graph_ins_edge(&graph, 9, 8, 4);
+
+  print_graph(&graph);
+
+  generic_list_t *span;
+  integer_mst(&graph, &span);
+
+  for (generic_list_elmt_t *elmt = generic_list_head(span); elmt != NULL; elmt = generic_list_next(elmt))
+  {
+    we_t *wEdge = generic_list_data(elmt);
+    printf("[ %d --(%.1f)-- %d ], ", wEdge->source, wEdge->weight, wEdge->destination);
+  }
+  
+  generic_list_destroy(span);
+  integer_graph_destroy(&graph);
   return EXIT_SUCCESS;
 }
--- TD6/exo2/game.c
+++ TD6/exo2/game.c
@@ -9,34 +9,38 @@
 #include "set.h"
 #include "stack.h"
 
-
-void print_graph (integer_graph_t* graph) {
-  generic_list_elmt_t* elem1 =
-    generic_list_head(&(graph->adjlists));
+void print_graph(integer_graph_t *graph)
+{
+  generic_list_elmt_t *elem1 =
+      generic_list_head(&(graph->adjlists));
   /* Boucle sur les elements de la liste */
-  for (; elem1 != NULL; elem1 = generic_list_next(elem1)) {
-    integer_adjlist_t* tempV1 =
-      (integer_adjlist_t*)generic_list_data(elem1);
-    printf ("Vertex %d: ", tempV1->vertex);
+  for (; elem1 != NULL; elem1 = generic_list_next(elem1))
+  {
+    integer_adjlist_t *tempV1 =
+        (integer_adjlist_t *)generic_list_data(elem1);
+    printf("Vertex %d: ", tempV1->vertex);
 
-    generic_list_elmt_t* elem2 =
-      generic_list_head(&(tempV1->adjacent));
+    generic_list_elmt_t *elem2 =
+        generic_list_head(&(tempV1->adjacent));
     /* Boucle sur les elements de l'ensemble (set) */
-    for (; elem2 != NULL; elem2 = generic_list_next(elem2)) {
+    for (; elem2 != NULL; elem2 = generic_list_next(elem2))
+    {
       integer_adjlist_elmt_t *tempV2 =
-	(integer_adjlist_elmt_t*)generic_list_data(elem2);
-      printf ("%d->", tempV2->vertex);
+          (integer_adjlist_elmt_t *)generic_list_data(elem2);
+      printf("%d->", tempV2->vertex);
     }
-    printf ("\n");
+    printf("\n");
   }
 }
 
-double traversabilite (int terrain) {
+double traversabilite(int terrain)
+{
 
   double cout = 0.0;
   /* printf ("%s: argument=%d\n", __func__, terrain); */
 
-  switch (terrain) {
+  switch (terrain)
+  {
   case 2:
     /* Route */
     cout = 1.0;
@@ -74,25 +78,29 @@ double traversabilite (int terrain) {
   return cout;
 }
 
-int buildGraph (integer_graph_t *graph, char* filename) {
-  FILE* fd = fopen (filename, "r");
-  if (fd == NULL) {
-    fprintf (stderr, "Ne peux pas ouvrir le fichier %s\n", filename);
+int buildGraph(integer_graph_t *graph, char *filename)
+{
+  FILE *fd = fopen(filename, "r");
+  if (fd == NULL)
+  {
+    fprintf(stderr, "Ne peux pas ouvrir le fichier %s\n", filename);
     return 0;
   }
 
   int dimension;
-  fscanf (fd, "%d", &dimension);
-  printf ("Dimension de la carte est %d\n", dimension);
+  fscanf(fd, "%d", &dimension);
+  printf("Dimension de la carte est %d\n", dimension);
 
-  for (int i = 0; i < dimension; i++) {
-    for (int j = 0; j < dimension+1; j++) {
-      char c  = fgetc(fd);
-      if (! isspace(c)) { 	/* Note: le saut de ligne est en debut */
-	/* double cout = traversabilite ( (int) (c - 48)); */
+  for (int i = 0; i < dimension; i++)
+  {
+    for (int j = 0; j < dimension + 1; j++)
+    {
+      char c = fgetc(fd);
+      if (!isspace(c))
+      { /* Note: le saut de ligne est en debut */
+        /* double cout = traversabilite ( (int) (c - 48)); */
 
-	/* TODO */
-
+        /* TODO */
       }
     }
   }
@@ -101,23 +109,25 @@ int buildGraph (integer_graph_t *graph, char* filename) {
   return dimension;
 }
 
-int main (int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
-  if (argc != 2) {
-    printf ("Usage: %s carte.txt\n", argv[0]);
+  if (argc != 2)
+  {
+    printf("Usage: %s carte.txt\n", argv[0]);
     return EXIT_FAILURE;
   }
 
   integer_graph_t graph;
-  integer_graph_init (&graph);
+  integer_graph_init(&graph);
 
   /* int dimension = buildGraph (&graph, argv[1]); */
 
-  print_graph (&graph);
+  print_graph(&graph);
 
   /* TODO */
 
-  integer_graph_destroy (&graph);
+  integer_graph_destroy(&graph);
 
   return EXIT_SUCCESS;
 }
